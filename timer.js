15f03f327824048f31194e8915ad3ed8482aba6b
= <=>
2014-04-21 17:20:57 -0500

    abort working

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@
 var initialTime = Date.now();
 var timeExpired = 0;
 var paused = 0;
+var aborted = 0;
 var timePausedAt = 0;
 var elapsedTime = 0;
 var timerTime = 20*60*1000;  //fixme - hard coded 20min
@@ -20,10 +21,14 @@ function resume(){
     document.getElementById("pauseButton").disabled = "";
     document.getElementById("resumeButton").disabled = "disabled";
 }
+function abort(){
+  aborted = 1;
+}
 
 function checkTime(){
   if (paused == 1) {return;}
   if (timeExpired == 1) {return;}
+  if (aborted == 1) {return;}
   var timeDifference = Date.now() - initialTime - elapsedTime;
   var timeLeft = (timerTime) - timeDifference;
   var formatted = convertTime(timeLeft);
@@ -40,8 +45,8 @@ function convertTime(miliseconds) {
     return ("0" + minutes).slice(-2) + ':' + ("0" + seconds).slice(-2);
   }
   else{
-    return "00:00 Time is up."; 
-    //"makes" time stop, need better solution maby
+    return "Time is up.";
+    timeExpired = 1; 
   }
 }
 // check time every 0.5 seconds
